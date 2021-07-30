c3926ce9327f43547acd4e9dc295214e03acad06
Simon <lim.simon10@gmail.com>
2021-07-31 01:42:26 +0800

    fixing logic to let shuffling work

--- src/gameComponents/Game.js
+++ src/gameComponents/Game.js
@@ -6,32 +6,42 @@ import deckofcards from '../deck-of-cards.js';
 
 
 const Game = () => {
-  //useEffect
+
   const [deck, setDeck] = useState(deckofcards);
 
   console.log(deck);
   console.log(Math.floor(Math.random() * 52));
   console.log("DECK test random card: " + JSON.stringify(deck[Math.floor(Math.random() * 52)]));
 
-
   const shuffleDeck = currentdeck => {
+    console.log("shuffleDeck will BEGIN now");
     let shuffleddeck = currentdeck;
-    for (let x = 1; x > 10; x++) {
-      shuffleddeck[Math.floor(Math.random() * 52)] = shuffleddeck[Math.floor(Math.random() * 52)];
-      console.log("shuffleddeck: " + shuffleddeck);
+
+    let randocard1 = Math.floor(Math.random() * 52) 
+    console.log("shuffleddeck[randocard1]: " + JSON.stringify(shuffleddeck[randocard1]))
+
+    for (var x = 1; x <= 100; x++) {
+      let randomcard1 = Math.floor(Math.random() * 52)
+      let randomcard2 = Math.floor(Math.random() * 52)
+      let duplicatecard1 = shuffleddeck[randomcard1]
+
+      shuffleddeck[randomcard1] = shuffleddeck[randomcard2]
+      shuffleddeck[randomcard2] = duplicatecard1
+
+      console.log("shuffleddeck[randomcard1]: " + shuffleddeck[randomcard1])
+      console.log("shuffleddeck[randomcard2]: " + shuffleddeck[randomcard2])
     }
 
-    console.log("deck will be shuffled now");
     setDeck(shuffleddeck);
+    console.log("shuffleddeck AFTER shuffling: " + JSON.stringify(shuffleddeck));
   };
 
   useEffect(() => {
-    return () => {
+      console.log("useEffect: deck test: " + JSON.stringify(deck)); 
+      console.log("useEffect: shuffleDeck will be TRIGGERED now");
+      shuffleDeck(deck); 
 
-     console.log("useEffect: deck will be shuffled now");
-      shuffleDeck(deck);
-    }
-  }, []);
+  });
 
 
   return (
