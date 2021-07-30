f5401fcfef109bc5a5a6c9c5db38b064212642a6
Simon <lim.simon10@gmail.com>
2021-07-30 16:23:22 +0800

    created functionality to shuffle deck

--- src/gameComponents/Game.js
+++ src/gameComponents/Game.js
@@ -14,6 +14,26 @@ const Game = () => {
   console.log("DECK test random card: " + JSON.stringify(deck[Math.floor(Math.random() * 52)]));
 
 
+  const shuffleDeck = currentdeck => {
+    let shuffleddeck = currentdeck;
+    for (let x = 1; x > 10; x++) {
+      shuffleddeck[Math.floor(Math.random() * 52)] = shuffleddeck[Math.floor(Math.random() * 52)];
+      console.log("shuffleddeck: " + shuffleddeck);
+    }
+
+    console.log("deck will be shuffled now");
+    setDeck(shuffleddeck);
+  };
+
+  useEffect(() => {
+    return () => {
+
+     console.log("useEffect: deck will be shuffled now");
+      shuffleDeck(deck);
+    }
+  }, []);
+
+
   return (
     <>
     </>
